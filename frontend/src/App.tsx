76128ffc88386418b9b9ec6208cc31939ef1b2a5
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-07-12 20:53:50 -0300

    Primeiro Componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./components/notificationButton"
+
 function App() {
   return (
-    <h1>Olá Mundo!</h1>
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
---
+++ frontend/src/components/notificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/icon-notification.svg'
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="botao-vermelho">
+            <img src={icon} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
