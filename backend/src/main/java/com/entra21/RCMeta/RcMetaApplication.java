e2315c93a57a590a3add0033c53a749f24efdf76
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-07-11 23:09:23 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/entra21/RCMeta/RcMetaApplication.java
@@ -0,0 +1,13 @@
+package com.entra21.RCMeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcMetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/entra21/RCMeta/RcMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entra21.RCMeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
