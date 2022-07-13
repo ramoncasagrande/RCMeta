f74ed74ea7e21c8c6ea3197a15821234d16bb8a0
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-07-12 22:23:26 -0300

    Implementado useState

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,25 +1,31 @@
-import NotificationButton from '../notificationButton'
+import NotificationButton from '../notificationButton';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import './styles.css'
+import './styles.css';
+import { useState } from 'react';
 
 function SalesCard() {
+
+    const [minDate, setMinDate] = useState(new Date());
+    const [maxDate, setMaxDate] = useState(new Date());
+
+
     return (
         <div className="card">
             <h2 className="vendasTitulo">Vendas</h2>
             <div>
                 <div className="form_container">
                     <DatePicker
-                        selected={new Date()}
-                        onChange={(date: Date) => { }}
+                        selected={minDate}
+                        onChange={(date: Date) => setMinDate(date)}
                         className="form"
                         dateFormat="dd/MM/yyyy"
                     />
                 </div>
                 <div className="form_container">
                     <DatePicker
-                        selected={new Date()}
-                        onChange={(date: Date) => { }}
+                        selected={maxDate}
+                        onChange={(date: Date) => setMaxDate(date)}
                         className="form"
                         dateFormat="dd/MM/yyyy"
                     />
@@ -43,11 +49,11 @@ function SalesCard() {
 
                     <tbody>
                         <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
+                            <td className="show992">#292</td>
+                            <td className="show576">12/06/2022</td>
+                            <td>Josimara</td>
+                            <td className="show992">29</td>
+                            <td className="show992">25</td>
                             <td>R$55300,00</td>
                             <td>
                                 <div className="botao-vermelho-container">
@@ -56,12 +62,12 @@ function SalesCard() {
                             </td>
                         </tr>
                         <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$55300,00</td>
+                            <td className="show992">#284</td>
+                            <td className="show576">02/04/2022</td>
+                            <td>Ramon</td>
+                            <td className="show992">38</td>
+                            <td className="show992">32</td>
+                            <td>R$67700,00</td>
                             <td>
                                 <div className="botao-vermelho-container">
                                     <NotificationButton />
@@ -69,12 +75,12 @@ function SalesCard() {
                             </td>
                         </tr>
                         <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$55300,00</td>
+                            <td className="show992">#286</td>
+                            <td className="show576">25/08/2022</td>
+                            <td>Mônica</td>
+                            <td className="show992">34</td>
+                            <td className="show992">29</td>
+                            <td>R$57800,00</td>
                             <td>
                                 <div className="botao-vermelho-container">
                                     <NotificationButton />
@@ -88,4 +94,4 @@ function SalesCard() {
     )
 }
 
-export default SalesCard
+export default SalesCard;
--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,6 +1,6 @@
-import icon from '../../assets/img/icon-notification.svg'
+import icon from '../../assets/img/icon-notification.svg';
 
-import './styles.css'
+import './styles.css';
 
 function NotificationButton() {
     return (
@@ -10,4 +10,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
