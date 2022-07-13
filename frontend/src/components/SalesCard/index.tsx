7a93e787fc99e716ff734cabcd29798792b2b90d
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-07-12 21:57:25 -0300

    Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,79 +1,91 @@
 import NotificationButton from '../notificationButton'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import './styles.css'
 
 function SalesCard() {
     return (
         <div className="card">
-                    <h2 className="vendasTitulo">Vendas</h2>
-                    <div>
-                        <div className="form_container">
-                            <input className="form" type="text" />
-                        </div>
-                        <div className="form_container">
-                            <input className="form" type="text" />
-                        </div>
-                    </div>
-
-                    <div>
-                        <table className="tabela">
-
-                            <thead>
-                                <tr>
-                                    <th className="show992">ID</th>
-                                    <th className="show576">Data</th>
-                                    <th>Vendedor</th>
-                                    <th className="show992">Visitas</th>
-                                    <th className="show992">Vendas</th>
-                                    <th>Total</th>
-                                    <th>Notificar</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                <tr>
-                                    <td className="show992">#341</td>
-                                    <td className="show576">08/07/2022</td>
-                                    <td>Anakin</td>
-                                    <td className="show992">15</td>
-                                    <td className="show992">11</td>
-                                    <td>R$55300,00</td>
-                                    <td>
-                                        <div className="botao-vermelho-container">
-                                            <NotificationButton />
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="show992">#341</td>
-                                    <td className="show576">08/07/2022</td>
-                                    <td>Anakin</td>
-                                    <td className="show992">15</td>
-                                    <td className="show992">11</td>
-                                    <td>R$55300,00</td>
-                                    <td>
-                                        <div className="botao-vermelho-container">
-                                            <NotificationButton />
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="show992">#341</td>
-                                    <td className="show576">08/07/2022</td>
-                                    <td>Anakin</td>
-                                    <td className="show992">15</td>
-                                    <td className="show992">11</td>
-                                    <td>R$55300,00</td>
-                                    <td>
-                                        <div className="botao-vermelho-container">
-                                            <NotificationButton />
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+            <h2 className="vendasTitulo">Vendas</h2>
+            <div>
+                <div className="form_container">
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
+                        className="form"
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
+                <div className="form_container">
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
+                        className="form"
+                        dateFormat="dd/MM/yyyy"
+                    />
+                </div>
+            </div>
+
+            <div>
+                <table className="tabela">
+
+                    <thead>
+                        <tr>
+                            <th className="show992">ID</th>
+                            <th className="show576">Data</th>
+                            <th>Vendedor</th>
+                            <th className="show992">Visitas</th>
+                            <th className="show992">Vendas</th>
+                            <th>Total</th>
+                            <th>Notificar</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">08/07/2022</td>
+                            <td>Anakin</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>R$55300,00</td>
+                            <td>
+                                <div className="botao-vermelho-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">08/07/2022</td>
+                            <td>Anakin</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>R$55300,00</td>
+                            <td>
+                                <div className="botao-vermelho-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576">08/07/2022</td>
+                            <td>Anakin</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>R$55300,00</td>
+                            <td>
+                                <div className="botao-vermelho-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
-  }
-  
-  export default SalesCard
+}
+
+export default SalesCard
